d34caadf105ab9753157812032bf4e2ea0909227
Nikhil Varma <nikhilvarma892@gmail.com>
2021-03-09 19:16:51 +0530

    Fix issue

--- dist/react-number-format.js
+++ dist/react-number-format.js
@@ -270,7 +270,8 @@
         afterDecimal = _splitDecimal.afterDecimal,
         hasNagation = _splitDecimal.hasNagation;
 
-    var roundedDecimalParts = parseFloat("0.".concat(afterDecimal || '0')).toFixed(scale).split('.');
+    var floatingString = parseFloat("0.".concat(afterDecimal || '0'));
+    var roundedDecimalParts = (afterDecimal.length < scale ? floatingString : floatingString.toFixed(scale)).split('.');
     var intPart = beforeDecimal.split('').reverse().reduce(function (roundedStr, current, idx) {
       if (roundedStr.length > idx) {
         return (Number(roundedStr[0]) + Number(current)).toString() + roundedStr.substring(1, roundedStr.length);
